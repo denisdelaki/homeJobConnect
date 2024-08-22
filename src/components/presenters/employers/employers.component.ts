71fdfbb5f2362c592977ec5393c4f2679ceea62a
denis.kipyegon@equitybank.co.ke <denis.kipyegon@equitybank.co.ke>
2024-08-22 11:53:53 +0300

    created the components

---
+++ src/components/presenters/employers/employers.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-employers',
+  standalone: true,
+  imports: [],
+  templateUrl: './employers.component.html',
+  styleUrl: './employers.component.scss'
+})
+export class EmployersComponent {
+
+}
