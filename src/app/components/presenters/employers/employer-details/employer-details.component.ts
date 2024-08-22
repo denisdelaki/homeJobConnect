633241877f6196763b8dd6cfe83919f5bb1607fb
denis.kipyegon@equitybank.co.ke <denis.kipyegon@equitybank.co.ke>
2024-08-22 12:19:25 +0300

    added more components

---
+++ src/app/components/presenters/employers/employer-details/employer-details.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-employer-details',
+  standalone: true,
+  imports: [],
+  templateUrl: './employer-details.component.html',
+  styleUrl: './employer-details.component.scss'
+})
+export class EmployerDetailsComponent {
+
+}
---
+++ src/app/components/presenters/employers/employer-list/employer-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-employer-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './employer-list.component.html',
+  styleUrl: './employer-list.component.scss'
+})
+export class EmployerListComponent {
+
+}
